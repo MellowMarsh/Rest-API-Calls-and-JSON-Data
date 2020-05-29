024459615c51f585b0204b60c8028e8e79ae6ed9
Marshalla Robinson <marshallarbnsn@gmail.com>
2020-05-29 10:19:45 -0500

    Added rest api to pull song lyrics from.

---
+++ test.py
@@ -0,0 +1,15 @@
+# Pulling Song from https://lyrics.ovh Lyrics from an API
+import json
+import requests
+
+artist = 'Jurassic 5'
+song_title = 'Quality Control'
+keywords = {'mood'}
+url = 'https://api.lyrics.ovh/v1/' + artist + '/' + song_title
+
+# fetch Lyrics
+response = requests.get(url)
+json_data = json.loads(response.content)
+lyrics = json_data['lyrics']
+
+print(lyrics)
